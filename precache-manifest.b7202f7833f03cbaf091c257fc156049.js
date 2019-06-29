18d66c58e656338b94a65dcefe103bef473a73da
nikitaushenko <nikitaushenko@gmail.com>
2019-06-29 21:41:56 +0300

    Updates

---
+++ precache-manifest.b7202f7833f03cbaf091c257fc156049.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2784f98d08ff0f11c1fd35a8e04ff8be",
+    "url": "/basic/index.html"
+  },
+  {
+    "revision": "31435b2b96775ddff225",
+    "url": "/basic/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/basic/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "31435b2b96775ddff225",
+    "url": "/basic/static/js/main.ebc246d7.chunk.js"
+  },
+  {
+    "revision": "11fc8ce1e7eaaf1fcde1",
+    "url": "/basic/static/js/runtime~main.bc9a8786.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/basic/static/media/logo.5d5d9eef.svg"
+  }
+]);
